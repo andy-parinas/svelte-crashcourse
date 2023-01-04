14ed79b669088098d4dafae0e6c9811795d147fb
Andy Parinas <andy.parinas@gmail.com>
2023-01-04 23:04:46 +1000

    improved on users

--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -27,14 +27,17 @@
 
   </script>
   
-  <div>
+  <div class="m-5">
         <h1 class="text-2xl text-center mt-10">List of Users</h1>
-        {#each getUsers() as user}
-        <div class="flex items-center my-4">
-            <img class="rounded-full w-20 mr-5" src="{user.image}" alt="" srcset="">
-            <p>{user.name}</p>
+        {#each getUsers() as user, i}
+        <div class="flex items-center my-4 bg-white border 
+                    p-2 rounded-lg shadow-md hover:shadow-lg cursor-pointer">
+            <p class="mr-4 w-10">{i + 1}</p>
+            <img class="rounded-full w-14 mr-5" src="{user.image}" alt="" srcset="">
+            <p class="mr-4 w-40">{user.name}</p>
+            <p class="w-20">{user.email}</p>
         </div>
         {:else}
-            <h2>No Users Found</h2>
+            <h2 class="text-xl text-center mt-10">No Users Found</h2>
         {/each}  
   </div>
