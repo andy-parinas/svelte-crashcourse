97d5cd207bba36706dd6b8d250027fe8bc0e69ef
Andy Parinas <andy.parinas@gmail.com>
2023-01-05 22:47:51 +1000

    Dispatch event

---
+++ src/lib/FilterUser.svelte
@@ -0,0 +1,20 @@
+<script>
+    import {createEventDispatcher} from 'svelte'
+    const dispatch = createEventDispatcher()
+
+    const filter = (e) => {
+        dispatch('filter', e.target.value)
+    }
+</script>
+
+
+<div class="flex items-center mb-5">
+    <h3 class="mr-4">Select User Type</h3>
+    <select on:change={filter}
+        name="user-filter" 
+        class="border rounded-lg px-5 py-1">
+        <option value="all">All</option>
+        <option value="active">Active</option>
+        <option value="inactive">Inactive</option>
+    </select>
+</div>
--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -1,36 +1,57 @@
 <script>
     import User from "./User.svelte";
     import user1 from '../assets/images/user1.png'
+    import FilterUser from "./FilterUser.svelte";
 
 
     const users = [
         {
             image: user1,
             name: "John Doe",
-            email: "john@email.com"
+            email: "john@email.com",
+            active: true
         },
         {
             image: user1,
             name: "Steve McQueen",
-            email: "steve@email.com"
+            email: "steve@email.com",
+            active: false
         },
         {
             image: user1,
             name: "Bill Gates",
-            email: "bill@email.com"
+            email: "bill@email.com",
+            active: true
         }
     ]
 
-
+    
     const getUsers = () => {
         return users
+    }
+
+    let filteredUsers = users
+    const filter = (event) => {
+        const filter = event.detail
+
+        filteredUsers = users.filter((user) => {
+            if(filter === 'all'){
+                return true
+            }else {
+                const active = filter === 'active'
+                return user.active === active
+            }
+
+        })
     }
 
   </script>
   
   <div class="m-5">
         <h1 class="text-2xl text-center mt-10">List of Users</h1>
-        {#each getUsers() as user, i}
+        <FilterUser on:filter={filter} />
+     
+        {#each filteredUsers as user, i}
         <User user={user} index={i} />
        
         {:else}
