af52bc557b56a7628b9d3bf64259fbe8d3e3e94e
Andy Parinas <andy.parinas@gmail.com>
2023-01-05 21:44:15 +1000

    nested component

---
+++ src/lib/User.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let user;
+    export let index;
+</script>
+
+ <div class="flex items-center my-4 bg-white border 
+        p-2 rounded-lg shadow-md hover:shadow-lg cursor-pointer">
+    <p class="mr-4 w-10">{index + 1}</p>
+    <img class="rounded-full w-14 mr-5" src="{user.image}" alt="" srcset="">
+    <p class="mr-4 w-40">{user.name}</p>
+    <p class="w-20">{user.email}</p>
+</div>
--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -1,4 +1,5 @@
 <script>
+    import User from "./User.svelte";
     import user1 from '../assets/images/user1.png'
 
 
@@ -30,13 +31,8 @@
   <div class="m-5">
         <h1 class="text-2xl text-center mt-10">List of Users</h1>
         {#each getUsers() as user, i}
-        <div class="flex items-center my-4 bg-white border 
-                    p-2 rounded-lg shadow-md hover:shadow-lg cursor-pointer">
-            <p class="mr-4 w-10">{i + 1}</p>
-            <img class="rounded-full w-14 mr-5" src="{user.image}" alt="" srcset="">
-            <p class="mr-4 w-40">{user.name}</p>
-            <p class="w-20">{user.email}</p>
-        </div>
+        <User user={user} index={i} />
+       
         {:else}
             <h2 class="text-xl text-center mt-10">No Users Found</h2>
         {/each}  
